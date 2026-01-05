4ef637e30410af823ad7782b570eba5fd3e861e4
Yann Fernandez Puig <yann.ferpuig@gmail.com>
2026-01-05 19:26:59 +0100

    Autotools

---
+++ deuteragonist/src/42sh.c
@@ -0,0 +1,7 @@
+#include "ast/ast.h"
+
+int main(void)
+{
+    print_ast();
+    return 0;
+}
---
+++ deuteragonist/src/ast/ast.c
@@ -0,0 +1,11 @@
+#include "ast.h"
+
+#include <stdio.h>
+
+#include "parser/parser.h"
+
+void print_ast(void)
+{
+    printf("ast !!!\n");
+    print_parser();
+}
