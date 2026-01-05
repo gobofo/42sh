d11eb7cac78cdd32095a02ea9c1a5a470b1693e4
Baptiste MATHIEU <baptiste.mathieu@epita.fr>
2026-01-05 23:56:02 +0100

    creation des fonctions et de l'AST

--- src/ast/ast.c
+++ src/ast/ast.c
@@ -4,6 +4,56 @@
 
 #include "parser/parser.h"
 
+struct AST *create_ast(enum types type, char *content){ //renvoi un arbre avec uniquement la racine "type"
+
+    struct AST *start = malloc(sizeof(struct AST));
+
+    start->type = type;
+    start->content = content;
+    start->count_children = 0;
+    start->children = NULL;
+    start->max_children = 0;
+
+    return start;
+
+};
+
+struct AST *add_children(struct AST *root, enum types type, char *content){ //ajoute un enfant en s'occupant des realloc
+
+    if (root->count_children >= root->max_children){ //pour les realloc
+        
+        if (root->max_children == 0){
+            root->max_children = 2;
+        }
+
+        else{
+            root->max_children *= 2;
+        }
+
+        root->children = realloc(root->children, root->max_children);
+    }
+
+    struct AST *new_child = create_ast(type, content);
+    root->children[root->count_children] = new_child;
+    root->count_children += 1;
+
+    return root;
+
+}
+
+void destroy_AST(struct AST *root){ //detruit l'AST (free)
+
+    for (int i = 0; i < root->count_children; i++){
+
+        destroy_AST(root->children[i]);
+
+    }
+
+    free(root->children);
+    free(root);
+
+}
+
 void print_ast(void)
 {
     printf("ast !!!\n");
--- src/ast/ast.h
+++ src/ast/ast.h
@@ -1,6 +1,32 @@
 #ifndef AST_H
 #define AST_H
 
+enum types{
+
+    PROGRAM,
+    LIST_STATEMENT,
+    STATEMENT,
+    IF_STATEMENT,
+    CONDITION,
+    BUILTIN,
+    ECHO_STATEMENT
+    VALUES
+
+};
+
+struct AST{
+
+    enum types type; //type de la node
+    char *content; //valeur de la node (si besoin)
+    struct AST **children; //tous les enfants
+    int count_children; //nb d'enfant
+    int max_children; //pour pouvoir realloc
+
+};
+
+struct AST *create_ast(enum types type, char *content);
+struct AST *add_children(struct AST *root, enum types type, char *content);
+void destroy_AST(struct AST *root);
 void print_ast(void);
 
 #endif /* ! AST_H */
