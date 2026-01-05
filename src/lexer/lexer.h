f0f9791f8fe3911f2d3e4d0960dbfb52d3c0940f
Yann Fernandez Puig <yann.ferpuig@gmail.com>
2026-01-05 22:58:21 +0100

    Start of lexer implementation

--- src/lexer/lexer.h
+++ src/lexer/lexer.h
@@ -1,6 +1,23 @@
 #ifndef LEXER_H
 #define LEXER_H
 
-void print_lexer(void);
+#include <stdio.h>
+#include <string.h>
+
+#include "../token.h"
+
+struct node
+{
+    struct token;
+
+    struct node *next;
+}
+
+struct token
+create_token(char *str);
+struct node *lexer(FILE *file);
+
+// Handle IO Entries
+// All must be a FILE
 
 #endif /* ! LEXER_H */
