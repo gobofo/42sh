cb3aedb611ef471845a112b1a00ffd88497abd43
Yann Sarthou <yann.sarthou@epita.fr>
2026-01-06 21:08:17 +0100

    ajout du debut de input

---
+++ src/IO/input/input.c
@@ -0,0 +1,20 @@
+#define _POSIX_C_SOURCE 200809L
+
+#include "input.h"
+#include <string.h>
+
+FILE *getInputFile(int argc, char* argv[]){
+	if(argc == 1){
+		return stdin;
+	}
+	if(argc!=3)
+		return NULL;
+
+	if(strcmp(argv[1], "-c") == 0)
+		return fmemopen(argv[2], strlen(argv[2]), "r");
+
+	if(strcmp(argv[1], "-e") == 0)
+		return fopen(argv[2], "r");
+
+	return NULL;
+}
