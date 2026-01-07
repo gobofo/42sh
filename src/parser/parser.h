5750e8dfb9693448892b85e96512e7f6d23a0bdd
Baptiste MATHIEU <baptiste.mathieu@epita.fr>
2026-01-07 22:58:17 +0100

    le parseur.h

--- src/parser/parser.h
+++ src/parser/parser.h
@@ -1,6 +1,20 @@
 #ifndef PARSER_H
 #define PARSER_H
 
-void print_parser(void);
+#include "../ast/ast.h"
+#include "../lexer/lexer.h"
+#include "../token.h"
+
+struct AST *input(struct token **token);
+struct AST *list(struct token **token);
+struct AST *and_or(struct token **token);
+struct AST *pipeline(struct token **token);
+struct AST *command(struct token **token);
+struct AST *shell_command(struct token **token);
+struct AST *rule_if(struct token **token);
+struct AST *else_clause(struct token **token);
+struct AST *compound_list(struct token **token);
+struct AST *simple_command(struct token **token);
+struct AST *element(struct token **token);
 
 #endif /* ! PARSER_H */
