fa336aba4e4a17800489849eb1faede3c32110e2
Yann Fernandez Puig <yann.ferpuig@gmail.com>
2026-01-08 16:09:51 +0100

    Expansion Clang-Format

--- src/expansion/expansion.c
+++ src/expansion/expansion.c
@@ -2,30 +2,36 @@
 
 #include "expansion.h"
 
-int expand(char **value){
-	char *p = *value;
+int expand(char **value)
+{
+    char *p = *value;
+    char *buff;
 
-	char *buff;
-	size_t size;
+    size_t size;
 
-	FILE *stream = open_memstream(&buff, &size);
+    FILE *stream = open_memstream(&buff, &size);
 
-	while(*p){
-		if(*p != '\''){
-			fputc(*p, stream);
-		}
-		if(*p == 0)
-			goto error;
-		p++;
-	}
-	
-	free(*value);
-	fclose(stream);
-	*value = buff;
-	return 0;
+    while (*p)
+    {
+        if (*p != '\'')
+            fputc(*p, stream);
+
+        if (*p == 0)
+            goto error;
+
+        p++;
+    }
+
+    fclose(stream);
+    free(*value);
+
+    *value = buff;
+
+    return 0;
 
 error:
-	fclose(stream);
-	free(buff);
-	return 1;
+    fclose(stream);
+    free(buff);
+
+    return 1;
 }
