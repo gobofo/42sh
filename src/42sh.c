9e4c46cae4f5a879bb3d57c931e9155b8e4105a0
lenny.taurines <lenny.taurines@epita.fr>
2026-01-08 01:25:39 +0100

    print-parser-fini

--- src/42sh.c
+++ src/42sh.c
@@ -1,46 +1,41 @@
+#include <stdio.h>
+
+#include "IO/input/input.h"
 #include "ast/ast.h"
 #include "lexer/lexer.h"
 #include "parser/parser.h"
-#include "IO/input/input.h"
 #include "token.h"
-
-#include <stdio.h>
-
 int main(int argc, char *argv[])
 {
-    FILE * file = getInputFile(argc, argv);
+    FILE *file = getInputFile(argc, argv);
 
-    if (file == NULL){
-
+    if (file == NULL)
+    {
         fprintf(stderr, "Error: IO\n");
         return 2;
-
     }
 
     struct token *tok = get_token(file);
 
     struct AST *ast;
 
-    if (tok == NULL){
-
+    if (tok == NULL)
+    {
         ast = create_ast(AST_LIST, NULL);
-
     }
 
-    else {
-
+    else
+    {
         ast = input(&tok);
 
-        if (ast == NULL){
-
+        if (ast == NULL)
+        {
             fprintf(stderr, "Error: Parsing\n");
             return 2;
-
         }
-
     }
-
-    //execution_ast(ast);
+    parser_print(ast);
+    // execution_ast(ast);
 
     destroy_AST(ast);
 
--- src/parser/print_parser.c
+++ src/parser/print_parser.c
@@ -3,72 +3,86 @@
 #include <stdlib.h>
 
 #include "../ast/ast.h"
+#include "parser.h"
 
-const char *get_string_of_node(struct AST *ast)
+char *get_string_of_node(struct AST *ast)
 {
-    static int _if = 1;
-    static int _cmd = 1;
-    static int _list = 1;
-    static int _value = 1;
-
-    static char buffer[64];
-
     switch (ast->rule)
     {
     case AST_IF:
-        snprintf(buffer, sizeof(buffer), "IF%d", _if++);
-        return buffer;
-
+        return "IF";
     case AST_CMD:
-        snprintf(buffer, sizeof(buffer), "CMD%d", _cmd++);
-        return buffer;
-
+        return "CMD";
     case AST_LIST:
-        snprintf(buffer, sizeof(buffer), "LIST%d", _list++);
-        return buffer;
-
+        return "LIST";
     case AST_VALUE:
-        snprintf(buffer, sizeof(buffer), "%s %d", ast->content, _list++);
-        return buffer;
-
+        return ast->content;
     default:
         return "";
     }
 }
+
+char *next(int suivant)
+{
+    static int c = 1;
+    if (suivant)
+    {
+        c++;
+    }
+    char *buffer = malloc(10);
+    sprintf(buffer, "%d", c);
+    return buffer;
+}
+
 void add(FILE *f, const char *texte)
 {
     fprintf(f, "%s", texte);
 }
-void parser_print_rec(struct AST *ast, FILE *f)
+void parser_print_rec(struct AST *ast, FILE *file_node, FILE *file_def_node)
 {
-    const char *node = get_string_of_node(ast);
+    char *node = next(0);
+
+    fprintf(file_def_node, "%s [label=\"%s\"];\n", node,
+            get_string_of_node(ast));
 
     for (int i = 0; i < ast->count_children; i++)
     {
-        const char *child = get_string_of_node(ast->children[i]);
-        add(f, "     ");
-        add(f, node);
-        add(f, " -> ");
-        add(f, child);
-        add(f, ";\n");
+        char *child = next(1);
+        add(file_node, "     ");
+        add(file_node, node);
+        add(file_node, " -> ");
+        add(file_node, child);
+        add(file_node, ";\n");
+        parser_print_rec(ast->children[i], file_node, file_def_node);
+        free(child);
     }
+    free(node);
+}
+void merge_into(FILE *src, FILE *dst)
+{
+    char buffer[1024];
 
-    for (int i = 0; i < ast->count_children; i++)
-    {
-        parser_print_rec(ast->children[i], f);
-    }
+    while (fgets(buffer, sizeof(buffer), src))
+        fputs(buffer, dst);
 }
 
 void parser_print(struct AST *ast)
 {
     if (ast == NULL)
         return;
-    FILE *f = fopen("AST.txt", "w"); // ecrase le fichier
+    FILE *file_node = fopen("AST_node.txt", "w"); // ecrase le fichier
+    FILE *file_def_node = fopen("AST_def_node.txt", "w"); // ecrase le fichier
 
-    add(f, "digraph Arbre {\n");
-    add(f, "node [shape=box, style=filled, fillcolor=lightblue];\n");
-    parser_print_rec(ast, f);
-    add(f, "}");
+    add(file_def_node, "digraph Arbre {\n");
+    add(file_def_node,
+        "node [shape=box, style=filled, fillcolor=lightblue];\n");
+    parser_print_rec(ast, file_node, file_def_node);
 
-    fclose(f);
+    fclose(file_node);
+    file_node = fopen("AST_node.txt", "r");
+    merge_into(file_node, file_def_node);
+
+    add(file_def_node, "}");
+    fclose(file_node);
+    fclose(file_def_node);
 }
