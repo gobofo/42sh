b28bdf8c0f449bacad89aad3e3ec4f1503a8e477
Yann Sarthou <yann.sarthou@epita.fr>
2026-01-12 10:38:39 +0100

    fix du echo, les flags marchaient plus

--- src/execution/execution.c
+++ src/execution/execution.c
@@ -11,22 +11,115 @@ int execute_node(struct AST *root);
  *
  * @return			Succes or Failure (0 or 1) of the function
  */
+
+int is_valid_echo_flags(char *flags){
+	if(flags[0]!='-') return 0;
+
+	int i=1;
+
+	while(flags[i]){
+		if(flags[i]!='n' && flags[i]!='e' && flags[i]!='E'){
+			return 0;
+		}
+		i++;
+	}
+	return 1;
+}
+
+int set_flags_echo(char** command,int* flag_e, int* flag_n){
+
+	int i=0;
+	while(command[i]!=NULL && is_valid_echo_flags(command[i])){
+		int j=1;
+		while(command[i][j]){
+			if(command[i][j]=='e'){
+				*flag_e=1;
+			}
+			if(command[i][j]=='E'){
+				*flag_e=0;
+			}
+			if (command[i][j]=='n'){
+				*flag_n=1;
+			}
+			j++;
+		}
+		i++;
+	}
+	return i;
+}
+
+void expand_backslash(char *str)
+{
+    int i = 0;
+    int j = 0;
+
+    while (str[i])
+    {
+        if (str[i] == '\\' && str[i + 1])
+        {
+            if (str[i + 1] == 'n')
+            {
+                str[j] = '\n';
+                i++;
+            }
+            else if (str[i + 1] == 't')
+            {
+                str[j] = '\t';
+                i++;
+            }
+            else if (str[i + 1] == '\\')
+            {
+                str[j] = '\\';
+                i++;
+            }
+            else
+            {
+                str[j] = str[i];
+				str[j+1] = str[i+1];
+				j++;
+				i++;
+            }
+        }
+        else
+        {
+            str[j] = str[i];
+        }
+        i++;
+        j++;
+    }
+    str[j] = '\0';
+}
+
+
 int my_echo(char **command)
 {
-    while (*command != NULL)
-    {
-        printf("%s", *command);
 
-        if (*(command + 1) != NULL)
-            printf(" ");
+	int flag_n = 0;
+	int flag_e = 0;
 
-        command++;
-    }
+	int i_debut = set_flags_echo(command, &flag_e, &flag_n);
 
-    printf("\n");
+	command+=i_debut;
 
-    return 0;
+	while (*command != NULL)
+	{
+		if(flag_e)
+			expand_backslash(*command);
+		printf("%s", *command);
+		
+		if (*(command + 1) != NULL)
+			printf(" ");
+
+		command++;
+	}
+
+	if(!flag_n)
+		printf("\n");
+
+	fflush(stdout);
+	return 0;
 }
+
 
 /**
  * @brief 		Creates the command with children from a node
