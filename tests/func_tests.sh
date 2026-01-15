3c6db3d4144e6ba189b18d1c8702182a6b13fb39
Baptiste MATHIEU <baptiste.mathieu@epita.fr>
2026-01-15 13:49:20 +0100

    testsuite v3

--- tests/func_tests.sh
+++ tests/func_tests.sh
@@ -11,16 +11,16 @@ TIMEOUT=5
 TOTAL=0
 SUCES=0
 
-if [ -z "$BIN_PATH" ]; then
-    echo "Error: BIN_PATH not set"
-    exit 1
-fi
+#if [ -z "$BIN_PATH" ]; then
+#    echo "Error: BIN_PATH not set"
+#    exit 1
+#fi
 
 test_cmd() {
     TOTAL=$((TOTAL + 1))
     
     local expected=$(timeout $TIMEOUT bash --posix -c "$1" 2>&1)
-    local actual=$(timeout $TIMEOUT "$BIN_PATH" -c "$1" 2>&1)
+    local actual=$(timeout $TIMEOUT ./src/42sh -c "$1" 2>&1) #$BIN_PATH
 
 	if [ "$expected" = "$actual" ]; then
 		echo -e "${GRN}$2${WHT}"
@@ -35,11 +35,13 @@ test_cmd() {
 
 if [ "$COVERAGE" = "yes" ]; then
     #test unitaires
+	echo "unitaire test"
 fi
 
 #test fonctionels
 
 test_cmd "echo a;" "Test echo super simple"
+test_cmd "echo a; echo b" "Test echo double"
 
 PERCENT=$(($SUCES * 100 / TOTAL))
 echo -e "${BBLU}Succeed:${GRN} $SUCES${WHT}"
@@ -50,4 +52,4 @@ if [ -n "$OUTPUT_FILE" ]; then
     echo "$PERCENT" > "$OUTPUT_FILE"
 fi
 
-exit(0)
+exit 0
