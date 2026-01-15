600071fd0216cd8d7ca52f72dbbeb2ace1935444
Yann Fernandez Puig <yann.ferpuig@gmail.com>
2026-01-15 11:40:58 +0100

    Commentaires Execution + Modularité

--- src/execution/execution.h
+++ src/execution/execution.h
@@ -1,16 +1,21 @@
 #ifndef EXECUTION_H
 #define EXECUTION_H
 
+#define _POSIX_C_SOURCE 200809L
+
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-#include "execute_command.h"
+#include "execute_command/execute_command.h"
 #include "my_pipe.h"
-#include "my_echo.h"
+#include "my_echo/my_echo.h"
+#include "my_redir/my_redir.h"
+
 #include "../ast/ast.h"
 #include "../parser/parser.h"
+#include "../expansion/expansion.h"
 
 int execute_ast(struct AST *root);
 
