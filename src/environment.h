a9e8021d6e4d1cd67bb4eef6a02be11f7d576ced
Yann Fernandez Puig <yann.ferpuig@gmail.com>
2026-01-15 16:53:37 +0100

    Clang format

--- src/environment.h
+++ src/environment.h
@@ -3,10 +3,10 @@
 
 #include "hash_map/hash_map.h"
 
-struct env{
-	struct hash_map *variables;
-	// we will have to add another hash_map for functions
+struct env
+{
+    struct hash_map *variables;
+    // we will have to add another hash_map for functions
 }
 
-
-#endif // ! ENVIRONMENT_H 
+#endif // ! ENVIRONMENT_H
