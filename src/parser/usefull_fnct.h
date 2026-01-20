a797a0ffdc0d6aea421dd54ca365cdab144a6c89
Baptiste MATHIEU <baptiste.mathieu@epita.fr>
2026-01-20 17:59:46 +0100

    update: creation usefull_fnct.h

---
+++ src/parser/usefull_fnct.h
@@ -0,0 +1,10 @@
+#ifndef USEFULL_FNCT_H
+#define USEFULL_FNCT_H
+
+/* ============= USEFULL FONCTION ============= */
+
+void eat_newlines(struct token **token);
+struct token *eat(struct token *token);
+bool is_valid_word(struct token *token);
+
+#endif /* ! USEFULL_FNCT_H */
