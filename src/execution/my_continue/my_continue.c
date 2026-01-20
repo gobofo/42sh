4aa1a1d4b8abecf617a0ee6316f27993bd993105
Yann Sarthou <sarthou.yann65@gmail.com>
2026-01-20 20:33:56 +0100

    update: continue

---
+++ src/execution/my_continue/my_continue.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "my_continue.h"
+extern struct env *env;
+
+int my_continue(char **command){
+  if(command[1]!=NULL){
+    fprintf(stderr, "Error: continue: too many arguments");
+    return 2;
+  }
+  env->continue_count=1;
+  if(command[0]){ // break X
+      int nb=atoi(command[0]);
+      if(nb==0){
+          fprintf(stderr, "Error: continue: not valid argument");
+          return 2;
+      }
+      env->continue_count=nb;
+  }
+  return env->last_exit_code;
+}
