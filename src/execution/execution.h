13402580d14f383d2192ef59607068654ac6368b
Yann Fernandez Puig <yann.ferpuig@gmail.com>
2026-01-20 14:05:18 +0100

    feat: Lookup table to execute nodes

--- src/execution/execution.h
+++ src/execution/execution.h
@@ -13,11 +13,10 @@
 #include "../expansion/expansion.h"
 #include "../hash_map/hash_map.h"
 #include "../parser/parser.h"
-
 #include "execute_command/execute_command.h"
+#include "my_cd/my_cd.h"
 #include "my_echo/my_echo.h"
 #include "my_redir/my_redir.h"
-#include "my_cd/my_cd.h"
 
 int execute_ast(struct AST *root);
 
