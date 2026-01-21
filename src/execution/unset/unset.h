ffe8fa5609fed396d60ec0d5e67d9fe558df64e0
Yann Fernandez Puig <yann.ferpuig@gmail.com>
2026-01-21 08:27:25 +0100

    style: clang format

--- src/execution/unset/unset.h
+++
@@ -1,9 +0,0 @@
-#ifndef UNSET_H
-#define UNSET_H
-#include "../../hash_map/hash_map.h"
-#include "../../environment/environment.h"
-#include "stdio.h"
-
-int unset(char** command);
-
-#endif // ! UNSET_H
