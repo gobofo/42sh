14806a741bdb4cb190ed93a2650f7315b4ca37d0
lenny.taurines <lenny.taurines@epita.fr>
2026-01-22 01:38:40 +0100

    fix : export like posix

--- src/execution/my_export/my_export.c
+++ src/execution/my_export/my_export.c
@@ -8,38 +8,33 @@ extern struct env *env;
 
 int my_export(char **command){
   if(command[0]==NULL){
-    fprintf(stderr, "Error: my_export: no args ");
+    fprintf(stderr, "Error: my_export: no args\n");
     return 2;
   }
-  if(strchr(command[0],'=')){
-
-    int i=0;
-    while(command[0][i]!='='){
-      i++;
-    }
-    command[0][i]=0;
-    if(!is_valid_name(command[0])){
-        fprintf(stderr, "Error: my_export: not valid Variable Name");
+  for(int j = 0; command[j] != NULL; j++){
+    if(strchr(command[j],'=')){
+      int i=0;
+      while(command[j][i]!='='){
+        i++;
+      }
+      command[j][i]=0;
+      if(!is_valid_name(command[j])){
+        fprintf(stderr, "Error: my_export: not valid Variable Name\n");
         return 2;
+      }
+      char* value=malloc(sizeof(char)* (strlen(&command[j][i+1])+1));
+      int pos=i+1;
+      int k=0;
+      
+      while(command[j][pos]){
+        value[k++]=command[j][pos++];
+      }
+      value[k]=0;
+      bool updated=0;
+      hash_map_insert(env->variables,command[j], value,&updated);
+      free(value);
     }
-
-    char* value=malloc(sizeof(char)* strlen(command[0]+i+1)+1);// pointeur apres le = dans strlen
-    int j=i+1;
-    int k=0;
-    
-    while(command[0][j]){
-      value[k++]=command[0][j++];
-    }
-    value[k]=0;
-
-
-    bool updated=0;
-    hash_map_insert(env->variables,command[0], value,&updated);
-    free(value);
-
+    export_add_variable(env->export_variables,strdup(command[j]));
   }
-  export_add_variable(env->export_variables,strdup(command[0]));
-
   return 0;
-
 }
