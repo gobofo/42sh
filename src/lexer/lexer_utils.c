8635e6b7c09330f6cea20773f32252a06d2779b5
Yann Fernandez Puig <yann.ferpuig@gmail.com>
2026-01-23 17:31:49 +0100

    fix: subshell token in lexer

--- src/lexer/lexer_utils.c
+++ src/lexer/lexer_utils.c
@@ -79,11 +79,14 @@ struct token *create_token(char *str)
         }
     }
 
-	if (str[0] == '$' && str[1] == '(')
-	{
-		token->type = SUBSHELL;
-		return token;
-	}
+    for (int i = 0; str[i + 1] != '\0'; i++)
+    {
+        if (str[i] == '$' && str[i + 1] == '(')
+        {
+            token->type = SUBSHELL;
+            return token;
+        }
+    }
 
     // If we get there then it is either an assignment either a random word
     char *delim = strchr(str, '=');
