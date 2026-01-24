b22b293ae3752867397246c5f6c84a3dfddc2ae5
Yann Fernandez Puig <yann.ferpuig@gmail.com>
2026-01-24 18:45:43 +0100

    update: lookup table for ast print

--- src/parser/print_parser.c
+++ src/parser/print_parser.c
@@ -1,97 +1,53 @@
 #include "../ast/ast.h"
 #include "print_parser.h"
 
+struct ast_node nodes[] =  {
+	{AST_LIST, "LIST", "#3498DB"},
+	{AST_SIMPLE_CMD, "SIMPLE_CMD", "#2ECC71"},
+	{AST_SHELL_CMD, "SHELL_CMD", "#E74C3C"},
+	{AST_IF, "IF", "#E67E22"},
+	{AST_WHILE, "WHILE", "#1ABC9C"},
+	{AST_UNTIL, "UNTIL", "#1ABC9C"},
+	{AST_FOR, "FOR", "#1ABC9C"},
+	{AST_VALUE, "VALUE", "#ECF0F1"},
+	{AST_AND, "AND", "#F1C40F"},
+	{AST_OR, "OR", "#F1C40F"},
+	{AST_PIPELINE, "PIPELINE", "#9B59B6"},
+	{AST_REDIR, "REDIR", "#E67E22"},
+	{AST_ASSIGNEMENT, "ASSIGNEMENT", "#ECF0F1"},
+	{AST_FUNC, "FUNC", "#E74C3C"},
+	{AST_SUB, "SUBSHELL", "#E67E22"},
+	{0, "", ""}
+};
+
 char *get_string_of_node(struct AST *ast)
 {
-    switch (ast->rule)
-    {
-    case AST_SHELL_CMD:
-        return "SHELL_CMD";
-    case AST_WHILE:
-        return "WHILE";
-    case AST_UNTIL:
-        return "UNTIL";
-    case AST_FOR:
-        return "FOR";
-    case AST_AND:
-        return "AND";
-    case AST_OR:
-        return "OR";
 
-    case AST_PIPELINE:
-        return "PIPELINE";
+	if (ast->rule != AST_VALUE && ast->rule != AST_ASSIGNEMENT &&
+			ast->rule != AST_FUNC)
+	{
+		return nodes[ast->rule].text;
+	}
 
-    case AST_REDIR:
-        return "REDIR";
-
-    case AST_IF:
-        return "IF";
-    case AST_SIMPLE_CMD:
-        return "SIMPLE_CMD";
-    case AST_LIST:
-        return "LIST";
-    case AST_VALUE:
-        return ast->content;
-
-    case AST_ASSIGNEMENT:
-        return ast->content;
-    case AST_FUNC:
-        return ast->content;
-    case AST_SUB:
-        return "SUBSHELL";
-    default:
-        return "";
-    }
+	return ast->content;
 }
+
 char *get_color_of_node(struct AST *ast)
 {
-    switch (ast->rule)
-    {
-    case AST_IF:
-        return "#E67E22";
-    case AST_SIMPLE_CMD:
-        return "#2ECC71";
-    case AST_LIST:
-        return "#3498DB";
-    case AST_VALUE:
-        return "#ECF0F1";
-    case AST_PIPELINE:
-        return "#9B59B6";
-
-    case AST_AND:
-        return "#F1C40F";
-    case AST_OR:
-        return "#F1C40F";
-
-    case AST_REDIR:
-        return "#E67E22";
-
-    case AST_UNTIL:
-        return "#1ABC9C";
-    case AST_WHILE:
-        return "#1ABC9C";
-    case AST_FOR:
-        return "#1ABC9C";
-    case AST_SHELL_CMD:
-        return "#E74C3C";
-    case AST_FUNC:
-        return "#E74C3C";
-    case AST_SUB:
-        return "#E67E22";
-
-    default:
-        return "";
-    }
+	return nodes[ast->rule].color;
 }
+
 char *next(int suivant)
 {
     static int c = 1;
+
     if (suivant)
-    {
         c++;
-    }
+
     char *buffer = malloc(20);
+
     sprintf(buffer, "%d", c);
+
     return buffer;
 }
 
@@ -99,6 +55,7 @@ void add(FILE *f, const char *texte)
 {
     fprintf(f, "%s", texte);
 }
+
 void parser_print_rec(struct AST *ast, FILE *file_node, FILE *file_def_node)
 {
     char *node = next(0);
@@ -119,6 +76,7 @@ void parser_print_rec(struct AST *ast, FILE *file_node, FILE *file_def_node)
     }
     free(node);
 }
+
 void merge_into(FILE *src, FILE *dst)
 {
     char buffer[1024];
