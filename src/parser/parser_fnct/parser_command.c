5297ca55fabae8df99da8e04d26d4352f0fe7ee4
Baptiste MATHIEU <baptiste.mathieu@epita.fr>
2026-01-27 10:58:00 +0100

    updat: fix clang-tidy parser_command.c

--- src/parser/parser_fnct/parser_command.c
+++ src/parser/parser_fnct/parser_command.c
@@ -74,6 +74,56 @@ err:
     return NULL;
 }
 
+//this fnct is here to handle the case 1 and 2
+//of shell command
+//'{' compound_list '}' and '(' compound_list ')'
+
+static struct AST *handle_subshell(struct lexer **lexer){
+
+    bool is_paren = get_current_type(*lexer) == L_PAREN;
+
+    *lexer = eat(*lexer);
+
+    if (!first_compound_list(get_current_token(*lexer)))
+    { // pas une coumpound list
+        return NULL;
+    }
+
+    struct AST *ast = compound_list(lexer);
+
+    if (ast == NULL)
+    { // remonte l'erreur
+        return NULL;
+    }
+
+    if (get_current_token(*lexer) == NULL
+        || (get_current_type(*lexer) != R_BRACE
+            && get_current_type(*lexer) != R_PAREN))
+    { // sans parenthese / bracket a la fin
+        goto err;
+    }
+
+    if ((get_current_type(*lexer) == R_PAREN && !is_paren)
+        || (get_current_type(*lexer) == R_BRACE && is_paren))
+    { // pas le meme correspondance ( } ou { )
+        goto err;
+    }
+
+    *lexer = eat(*lexer);
+    if (is_paren) // subshell
+    {
+        struct AST *ast_sub_shell = create_ast(AST_SUB, NULL);
+        ast = add_children(ast_sub_shell, ast);
+    }
+
+    return ast;
+
+err:
+    destroy_AST(ast);
+    return NULL;
+
+}
+
 //(10) shell_c
 //    '{' compound_list '}'
 //   |'(' compound_list ')'
@@ -117,46 +167,8 @@ struct AST *shell_command(struct lexer **lexer)
                  || get_current_type(*lexer) == L_PAREN))
     { // cas 1 et 2
 
-        bool is_paren = get_current_type(*lexer) == L_PAREN;
+        return handle_subshell(lexer);
 
-        *lexer = eat(*lexer);
-
-        if (!first_compound_list(get_current_token(*lexer)))
-        { // pas une coumpound list
-            return NULL;
-        }
-
-        struct AST *ast = compound_list(lexer);
-
-        if (ast == NULL)
-        { // remonte l'erreur
-            return NULL;
-        }
-
-        if (get_current_token(*lexer) == NULL
-            || (get_current_type(*lexer) != R_BRACE
-                && get_current_type(*lexer) != R_PAREN))
-        { // sans parenthese / bracket a la fin
-            goto err;
-        }
-
-        if ((get_current_type(*lexer) == R_PAREN && !is_paren)
-            || (get_current_type(*lexer) == R_BRACE && is_paren))
-        { // pas le meme correspondance ( } ou { )
-            goto err;
-        }
-
-        *lexer = eat(*lexer);
-        if (is_paren) // subshell
-        {
-            struct AST *ast_sub_shell = create_ast(AST_SUB, NULL);
-            ast = add_children(ast_sub_shell, ast);
-        }
-
-        return ast;
-    err:
-        destroy_AST(ast);
-        return NULL;
     }
     else
     {
@@ -164,13 +176,13 @@ struct AST *shell_command(struct lexer **lexer)
     }
 }
 
-//(18) simple_command =
-//         prefix {prefix}
-//(19)    | {prefix} ( WORD | SUBSHELL ) { element }
 
-struct AST *simple_command(struct lexer **lexer)
-{
-    struct AST *ast = create_ast(AST_SIMPLE_CMD, NULL);
+//This function is here to handle all the prefix in the
+//simple_command function
+//it return 0 if it didn't find any prefix
+//and 1 if it find 1 or more prefix
+
+static int eat_prefix(struct lexer **lexer, struct AST *ast){
 
     int pref = 0;
 
@@ -181,9 +193,76 @@ struct AST *simple_command(struct lexer **lexer)
         pref = 1;
 
         struct AST *child = prefix(lexer);
+
         if (child == NULL) // remonte l'erreur
-            goto err;
+            return -1;
+
         ast = add_children(ast, child);
+    }
+
+    return pref;
+
+}
+
+//this function's goal is respect the rule 2 of simple_command
+//in fact, it eat the first Word or subshell
+//and after it eat all the element
+//if there is an issue it return -1
+//else it return 0
+
+static int eat_word_and_element(struct lexer **lexer, struct AST *ast){
+
+    struct AST *child;
+
+    if (!is_valid_word(*lexer))
+    { // pas un mot alors c une erreur
+        return -1;
+    }
+
+    child = create_ast(
+        AST_VALUE,
+        strdup(get_current_content(*lexer))); // prend la valeur du WORDS
+
+    ast = add_children(ast, child);
+
+    // pour gerer les subshell
+
+    if (get_current_type(*lexer) == SUBSHELL && !verif_subshell(*lexer))
+    {
+        return -1;
+    }
+
+    *lexer = eat(*lexer);
+
+    while (
+        get_current_token(*lexer)
+        && (first_element(get_current_token(*lexer))
+            || is_valid_word(*lexer))) // first de element ou un valid word
+                                           // car element peut etre just word
+    {
+        struct AST *child_el = element(lexer);
+        if (child_el == NULL) // remonte l'err
+            return -1;
+
+        ast = add_children(ast, child_el);
+    }
+
+    return 0;
+
+}
+
+//(18) simple_command =
+//         prefix {prefix}
+//(19)    | {prefix} ( WORD | SUBSHELL ) { element }
+
+struct AST *simple_command(struct lexer **lexer)
+{
+    struct AST *ast = create_ast(AST_SIMPLE_CMD, NULL);
+
+    int pref = eat_prefix(lexer, ast);
+
+    if (pref == -1){
+        goto err;
     }
 
     if (pref == 0
@@ -195,59 +274,20 @@ struct AST *simple_command(struct lexer **lexer)
     if (pref == 0
         || (get_current_token(*lexer) && is_valid_word(*lexer))) // regle 2
     {
-        struct AST *child;
 
-        if (!is_valid_word(*lexer))
-        { // pas un mot alors c une erreur
+        if (eat_word_and_element(lexer, ast) == -1){ //mange le word | subshell puis {element}
             goto err;
         }
 
-        child = create_ast(
-            AST_VALUE,
-            strdup(get_current_content(*lexer))); // prend la valeur du WORDS
-
-        ast = add_children(ast, child);
-
-        // pour gerer les subshell
-
-        if (get_current_type(*lexer) == SUBSHELL && !verif_subshell(*lexer))
-        {
-            goto err;
-        }
-
-        *lexer = eat(*lexer);
-
-        while (
-            get_current_token(*lexer)
-            && (first_element(get_current_token(*lexer))
-                || is_valid_word(*lexer))) // first de element ou un valid word
-                                           // car element peut etre just word
-        {
-            struct AST *child_el = element(lexer);
-            if (child_el == NULL) // remonte l'err
-                goto err;
-
-            ast = add_children(ast, child_el);
-        }
-
-        if (!follow_simple_command(get_current_token(
-                *lexer))) // si ce qui suit n'est pas bon direct erreur
-        {
-            goto err;
-        }
-
-        return ast;
     }
-    else // cas 1
+
+    if (!follow_simple_command(get_current_token(
+            *lexer))) // si ce qui suit n'est pas bon direct erreur
     {
-        if (!follow_simple_command(get_current_token(
-                *lexer))) // si ce qui suit n'est pas bon direct erreur
-        {
-            goto err;
-        }
-
-        return ast;
+        goto err;
     }
+
+    return ast;
 
 err:
     destroy_AST(ast);
