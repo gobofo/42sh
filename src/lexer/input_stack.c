9d4b327db73185bf8d948ad1169a61f7b50373f6
Yann Fernandez Puig <yann.ferpuig@gmail.com>
2026-01-28 09:43:50 +0100

    update: push and pop funcs for input_stack

--- src/lexer/input_stack.c
+++ src/lexer/input_stack.c
@@ -13,20 +13,38 @@ struct input_stack *init_input_stack(FILE *input)
 	return stack;
 }
 
+void push_input_stack(struct input_stack **stack, FILE *input, char *alias_name)
+{
+	struct input_stack *new_input = malloc(sizeof(struct input_stack));
+	if (new_input == NULL)
+		return;
+
+	new_input->file = input;
+	new_input->alias_name = alias_name ? strdup(alias_name) : NULL;
+	new_input->next = *stack;
+
+	*stack = new_input;
+}
+
+void pop_input_stack(struct input_stack **stack)
+{
+	if (stack == NULL || *stack == NULL)
+		return;
+
+	struct input_stack *top = *stack;
+	*stack = top->next;
+
+	if (top->alias_name)
+	{
+		fclose(top->file);
+		free(top->alias_name);
+	}
+
+	free(top);
+}
+
 void free_input_stack(struct input_stack *stack)
 {
-	struct input_stack *cur = stack;
-	while (cur)
-	{
-		struct input_stack *temp = cur;
-		cur = cur->next;
-
-		if (temp->alias_name)
-		{
-			fclose(temp->file);
-			free(temp->alias_name);
-		}
-
-		free(temp);
-	}
+	while (stack != NULL)
+		pop_input_stack(&stack);
 }
